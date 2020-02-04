f8818c571dbf1dab7ab4b9ad6fcbe3a4eacf2681
Bastien Secher <ba.secher@nameshield.net>
2020-02-04 09:48:25 +0100

    change{mylittlepwnie.sh}: Add the unicorn

--- mylittlepwnie.sh
+++ mylittlepwnie.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #
-# This little licorn just check for verb-tempering on a specific endpoint
+# This little unicorn just check for verb-tempering on a specific endpoint
 #
 # Owner : Baxytra
-
-set -euo pipefail
-
-# Script vars
-url="${1}"
 
 usage() {
 	# Present the script usage
 	
-	echo 'This little licorn just check for verb-tempering on a specific endpoint'
+	echo 'This little unicorn just check for verb-tempering on a specific endpoint'
 	echo "Usage :  $(basename "${0}") <URL>"
+}
+
+callUnicorn() {
+	# Hey ! Unicorn !
+	b64Unicorn="ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgLwogICAgICAgICAgICAgICAgICAgX18gICAgICAgLy8KICAgICAgICAgICAgICAgICAgIC1cPSBcPVwgLy8KICAgICAgICAgICAgICAgICAtLT1fXD0tLS0vLz0tLQogICAgICAgICAgICAgICAtXz09LyAgXC8gLy9cLy0tCiAgICAgICAgICAgICAgICA9PS8gICAvTyAgIE9cPT0tLQogICBfIF8gXyBfICAgICAvXy8gICAgXCAgXSAgLy0tCiAgL1wgKCAoLSBcICAgIC8gICAgICAgXSBdIF09PS0KIChcIF9cX1xfXC1cX18vICAgICBcICAoLF8sKS0tCihcXy8gICAgICAgICAgICAgICAgIFwgICAgIFwtClwvICAgICAgLyAgICAgICAoICAgKCBcICBdIC8pCi8gICAgICAoICAgICAgICAgXCAgIFxfIFwuLyApCiggICAgICAgXCAgICAgICAgIFwgICAgICApICBcCiggICAgICAgL1xfIF8gXyBfIC8tLS0vIC9cXyAgXAogXCAgICAgLyBcICAgICAvIF9fX18vIC8gICBcICBcCiAgKCAgIC8gICApICAgLyAvICAvX18gKSAgICggICkKICAoICApICAgLyBfXy8gJy0tLWAgICAgICAgLyAvCiAgXCAgLyAgIFwgXCAgICAgICAgICAgICBfLyAvCiAgXSBdICAgICApX1xfICAgICAgICAgL19fXC8KICAvX1wgICAgIF1fX19cCiAoX19fKQo=" 
+	echo "${b64Unicorn}"| base64 -d
+	echo -e "\n\n"
 }
 
 currentPageStatus() {
@@ -39,5 +41,12 @@ checkVerbTempering() {
 	done
 }
 
+if [ -z $1 ]; then
+	usage
+	exit 0
+fi
+
+url="${1}"
+callUnicorn
 currentPageStatus "${url}"
 checkVerbTempering "${url}"
